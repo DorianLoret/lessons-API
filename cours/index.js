7bb32899778e65783f1bb3082d2f220d14548f40
Dorian Loret <dorian.loret20@gmail.com>
2022-06-24 19:32:02 +0200

    cooking-app

--- cours/index.js
+++ cours/index.js
@@ -65,19 +65,68 @@ setTimeout(() => {
 }, 2000);
 
 // Promise
-fetch('monlien').then((res) => res)
+// fetch("monlien").then((res) => res);
 
 // async/await
-async function fetchData {
-  await fetch('monlien')
+async function fetchData() {
+  await fetch("monlien");
   // attend que le await soit exécuté avant de faire la suite
 
   executeFonction();
 }
 
 const fetchData2 = async () => {
-  await fetch('monlien')
+  await fetch("monlien");
   // attend que le await soit exécuté avant de faire la suite
 
   executeFonction();
-}
+};
+
+//--------
+// LE JSON
+//--------
+
+// Méthode .json() => méthode qui s'auto-résout en royant le Body de la requête.
+
+fetch("data.json")
+  .then((res) => res.json())
+  .then((data) => {
+    // Stringify => convertit en JSON
+    let settings = JSON.stringify(data);
+    // parse => Transforme JSON en object JS
+    // console.log(JSON.parse(settings));
+  });
+
+//--------
+// Web API
+//--------
+
+// CLIENT STORAGE
+//---------------
+
+// Local Storage
+localStorage.data = "Je stocke la data";
+// document.body.Textcontent = localStorage.data;
+localStorage.removeItem("data");
+
+const obj = {
+  name: "Denis",
+  age: 22,
+};
+
+// Il faut passer des chaînes de caractères
+localStorage.user = JSON.stringify(obj);
+// console.log(JSON.parse(localStorage.user));
+
+//----------------
+// Session Storage
+sessionStorage.dataSettings = "55px";
+sessionStorage.clear();
+
+//--------
+// Cookies
+document.cookie = "username=Ajani";
+
+// Bonne pratique
+
+document.cookie = "pseudo=AJ;path=/;max-age=450000;secure;samesite";
